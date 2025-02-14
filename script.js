9cb335c22ff62bd3c614a1534a3728a396ba78a9
mohamedmuqsith <mohamedmukshidh35@gmail.com>
2025-02-14 21:21:22 +0530

    updated responsive slidbar

--- script.js
+++ script.js
@@ -23,36 +23,35 @@ closenav.addEventListener('click',function(){
 
 // slider-container
 
-var sliderContainer=document.getElementById('sliderContainer')
-var slider=document.getElementById('slider')
-var card=slider.getElementsByTagName('div')
+var slider = document.getElementById("slider");
+var slides = slider.getElementsByTagName("div");
+var currentIndex = 0;
+var totalSlides = slides.length;
 
-var elementToShow=3;
-if(document.body.clientWidth<1000){
-    var elementToShow=1;
+// Detect screen size for responsiveness
+var elementsToShow = window.innerWidth < 1000 ? 1 : 1; // Show 1 element at a time
+var sliderWidth = document.getElementById("sliderContainer").clientWidth;
+var slideWidth = sliderWidth / elementsToShow;
+
+// Set slider width dynamically
+slider.style.width = totalSlides * slideWidth + "px";
+
+for (let i = 0; i < slides.length; i++) {
+    slides[i].style.width = slideWidth + "px";
 }
 
-
-var sliderContainerWidth=sliderContainer.clientWidth
-var cardWidth=sliderContainerWidth/elementToShow
-
-slider.style.width=card.length*cardWidth+'px'
-slider.style.transition='margin'
-slider.style.transitionDuration='1s'
-
-for (let index = 0; index < card.length; index++) {
-    const element = card[index];
-    element.style.width=cardWidth+'px'
-    
+// Function to slide to previous image
+function prv() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
 }
 
-function prv(){
-    if(+slider.style.marginLeft.slice(0,-2)!=cardWidth*(card.length-elementToShow))
-    slider.style.marginLeft =  ((+slider.style.marginLeft.slice(0,-2))-cardWidth)+'px'
-
-}
-function next(){
-    if(+slider.style.marginLeft.slice(0,-2)!=0)
-        slider.style.marginLeft =  ((+slider.style.marginLeft.slice(0,-2))+cardWidth)+'px'
-
+// Function to slide to next image
+function next() {
+    if (currentIndex < totalSlides - elementsToShow) {
+        currentIndex++;
+        slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+    }
 }
